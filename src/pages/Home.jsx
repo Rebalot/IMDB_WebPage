8df69a2721b741e91350c2cdbf7212f27ebd8566
Alberto <alberto_a_e@outlook.com>
2024-08-20 20:39:40 -0600

    Add carousel data feature: implement data transformation for movie trailers

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,10 +5,20 @@ import styles from '../assets/styles/Home.module.css'
 import Carousel from "../components/Carousel";
 
 const Home = () => {
-    const urlMovie = 'https://api.themoviedb.org/3/discover/movie'
+    const urlMovie = 'https://api.themoviedb.org/3/discover/movie';
+    const urlTV = 'https://api.themoviedb.org/3/discover/tv';
+
+    const [listaMovieProximas, setListaMovieProximas] = useState([]);
+    const [listaMovieTrailers, setListaMovieTrailers] = useState([]);
+    const [listaTVProximas, setListaTVProximas] = useState([]);
+    const [listaTVTrailers, setListaTVTrailers] = useState([]);
+    const [carouselData, setCarouselData] = useState([]);
     useEffect(() => {
-        async function obtenerMoviesTendencia() {
-            const newUrl = `${urlMovie}`;
+        obtenerMoviesProximas();
+        obtenerTVProximas();
+        async function obtenerMoviesProximas() {
+            const newUrl = `${urlMovie}?include_adult=false&include_video=false&language=en-US&page=1&primary_release_date.gte=2024-08-01&sort_by=popularity.desc`;
+            //primary_release_date.gte (las estrenadas o próximas arriba de una fecha ej. 2024-08-01)
             const options = {
                 method: 'GET',
                 headers: {
@@ -21,35 +31,88 @@ const Home = () => {
                 if (!response.ok) {
                     throw new Error('Error al obtener data');
                 }
-                const dataObtenida = await response.json();
-                console.log('Data obtenida correctamente');
-                return dataObtenida;
+                const data = await response.json();
+                const listaMovies = data.results;
+                setListaMovieProximas(listaMovies);
+                
             } catch (error) {
                 console.error('Error al obtener data', error);
             }
         }
-
-        obtenerMoviesTendencia();
-
-        async function obtenerTrailersTendenciaYoutube() {
-            const apiKey = 'AIzaSyCgljdJ1kdRAmv2mrHhbEhFE1ZDjL0HCE8';
-            const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=US&videoCategoryId=1&key=${apiKey}`;
-
-            fetch(url)
-            .then(response => response.json())
-            .then(data => console.log(data))
-            .catch(error => console.error(error));
+        async function obtenerTVProximas() {
+            const newUrl = `${urlTV}?first_air_date.gte=2024-08-01&include_adult=false&include_null_first_air_dates=false&language=en-US&page=1&sort_by=popularity.desc&with_origin_country=US`;
+            //first_air_date.gte (las estrenadas o próximas arriba de una fecha ej. 2024-08-01), with_origin_country (US, para evitar que hayan muchos resultados de origen cn, kr, jp que no suelen ser de gusto general)
+            const options = {
+                method: 'GET',
+                headers: {
+                    accept: 'application/json',
+                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDEzNjFjN2U4MzQzYmU5NTdiNGE1MGU1OWIxNzNiZiIsIm5iZiI6MTcyMzE3MzgxOC4wMzYxMDUsInN1YiI6IjY2YjQzNTFmYjJkMWM1NWM3OTZmMjNmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L24aOuGVERuuekN14gSCAXJMte02hky1GALzw9O1w4o'
+                }
+            };
+            try {
+                const response = await fetch(newUrl, options);
+                if (!response.ok) {
+                    throw new Error('Error al obtener data');
+                }
+                const data = await response.json();
+                setListaTVProximas(data.results);
+            } catch (error) {
+                console.error('Error al obtener data', error);
+            }
         }
     }, []);
+    useEffect(() => {
+        obtenerMovieVideos();
+        async function obtenerMovieVideos() {
+            if (listaMovieProximas.length === 0) return;
+            // Iterar sobre cada película para obtener todos los videos disponibles y posteriormente obtener unicamente el que sea el trailer oficial
+            const movieVideosPromises = listaMovieProximas.map(async (movie) => {
+                const videosUrl = `https://api.themoviedb.org/3/movie/${movie.id}/videos`;
+                const options = {
+                    method: 'GET',
+                    headers: {
+                        accept: 'application/json',
+                        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNDEzNjFjN2U4MzQzYmU5NTdiNGE1MGU1OWIxNzNiZiIsIm5iZiI6MTcyMzE3MzgxOC4wMzYxMDUsInN1YiI6IjY2YjQzNTFmYjJkMWM1NWM3OTZmMjNmMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.L24aOuGVERuuekN14gSCAXJMte02hky1GALzw9O1w4o'
+                    }
+                };
+                const videosResponse = await fetch(videosUrl, options);
+                if (!videosResponse.ok) {
+                    throw new Error('Error al obtener detalles de la película');
+                }
+                const videosData = await videosResponse.json();
+
+                // Buscar el trailer oficial
+                const officialTrailer = videosData.results.find((video) => 
+                    video.type === "Trailer" && video.name === "Official Trailer"
+                );
+                //Matiene las propiedades originales y agrega la propiedad official_trailer, en caso de no contar con uno, el value será null.
+                return {
+                    ...movie,
+                    official_trailer: officialTrailer ? officialTrailer : null
+                };
+            });
     
-    const items = [
-        { title: 'Alien', image: "https://linktoimage1.com", trailerUrl: "https://www.youtube.com/watch?v=cj85e-1tgyI&t=416s" },
-        { title: 'Alien',  image: "https://linktoimage2.com", trailerUrl: "https://www.youtube.com/embed/trailer2" },
-        { title: 'Alien',  image: "https://linktoimage3.com", trailerUrl: "https://www.youtube.com/embed/trailer3" },
-        { title: 'Alien',  image: "https://linktoimage1.com", trailerUrl: "https://www.youtube.com/embed/trailer1" },
-        { title: 'Alien',  image: "https://linktoimage2.com", trailerUrl: "https://www.youtube.com/embed/trailer2" },
-        { title: 'Alien',  image: "https://linktoimage3.com", trailerUrl: "https://www.youtube.com/embed/trailer3" },
-    ];
+            const updatedMovies = await Promise.all(movieVideosPromises);
+    
+            setListaMovieTrailers(updatedMovies);
+        }
+    }, [listaMovieProximas]);
+    useEffect(() => {
+        console.log('TV Próximas:', listaTVProximas);
+    }, [listaTVProximas]);
+    useEffect(() => {
+        console.log("Movie trailers: ", listaMovieTrailers);
+        const moviesConTrailer = listaMovieTrailers.filter((movie)=> movie.official_trailer !== null);
+        const dataEndpoints = moviesConTrailer.map((movie) => {
+            return{
+                title: movie.original_title,
+                image: `https://media.themoviedb.org/t/p/w1920_and_h427_multi_faces/${movie.backdrop_path}`,
+                trailerUrl: `https://www.youtube.com/watch?v=${movie.official_trailer.key}`
+            };
+        });
+
+        setCarouselData(dataEndpoints);
+    }, [listaMovieTrailers]);
     return (
         <>
             <main>
@@ -58,7 +121,7 @@ const Home = () => {
                         <h1>Welcome!</h1>
                         <h2>Millions of movies,TV shows and people to discover. Explore now.</h2>
                         <div>
-                            <Carousel items={items}></Carousel>
+                            <Carousel items={carouselData}></Carousel>
                         </div>
                     </div>
                     <SearchBar></SearchBar>
