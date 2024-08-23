e71ae8cd40a559776fcd29777952f4f0cbc5fb40
fcoaguilarjt <fcoaguilar.jt@gmail.com>
2024-08-23 13:06:07 -0600

    Estilos de populares

--- src/pages/Movie_Popular.jsx
+++ src/pages/Movie_Popular.jsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import SearchBar from "../components/SearchBar";
 import { NavLink } from "react-router-dom";
 import styles from '../assets/styles/Home.module.css'
-import Carousel from "../components/Carousel";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-const Popular = () => {
-    const urlMovie = 'https://api.themoviedb.org/3/movie/popular'
+const Movie_Popular = () => {
 
     const [listaPeliculas, setListaPeliculas] = useState([])
-
     useEffect(() => {
         const consultarPeliculas = async () => {
             const url = "https://api.themoviedb.org/3/movie/popular"
@@ -32,25 +33,11 @@ const Popular = () => {
         consultarPeliculas();
 
 
-        /*    async function obtenerTrailersTendenciaYoutube() {
-                const apiKey = 'AIzaSyCgljdJ1kdRAmv2mrHhbEhFE1ZDjL0HCE8';
-                const url = `https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&regionCode=US&videoCategoryId=1&key=${apiKey}`;
-    
-                fetch(url)
-                    .then(response => response.json())
-                    .then(data => console.log(data))
-                    .catch(error => console.error(error));
-            }*/
     }, []);
 
-    /*  const items = [
-          { title: 'Alien', image: "https://linktoimage1.com", trailerUrl: "https://www.youtube.com/watch?v=cj85e-1tgyI&t=416s" },
-          { title: 'Alien', image: "https://linktoimage2.com", trailerUrl: "https://www.youtube.com/embed/trailer2" },
-          { title: 'Alien', image: "https://linktoimage3.com", trailerUrl: "https://www.youtube.com/embed/trailer3" },
-          { title: 'Alien', image: "https://linktoimage1.com", trailerUrl: "https://www.youtube.com/embed/trailer1" },
-          { title: 'Alien', image: "https://linktoimage2.com", trailerUrl: "https://www.youtube.com/embed/trailer2" },
-          { title: 'Alien', image: "https://linktoimage3.com", trailerUrl: "https://www.youtube.com/embed/trailer3" },
-      ];*/
+
+
+
     return (
         <>
             <main>
@@ -58,24 +45,32 @@ const Popular = () => {
                     <div className={styles.welcome_wrapper}>
                         <h1>Bienvenido</h1>
                         <h2>Peliculas populares</h2>
-                        <div>
-                            <ul>
+                        <Row style={{gap: '20px'}}>
                             {
-                listaPeliculas.map((pelicula, index) => {
-                    return (
-                        <li key={index}>{pelicula.title.charAt(0).toUpperCase()+pelicula.title.slice(1)}</li>
-                    )
-                })
-            }
-                            </ul>
+                                listaPeliculas.map((pelicula, index) => {
+                                    const imageURL = `https://media.themoviedb.org/t/p/w440_and_h660_face/${pelicula.backdrop_path}`
+                                    return (
+                                        <Col key={index}>
+                                        <Card style={{ width: '200px', height: '450px'}} >
+                                        <Card.Img variant="top" src={imageURL} />
+                                        <Card.Body>
+                                            <Card.Title>{pelicula.title.charAt(0).toUpperCase() + pelicula.title.slice(1)}</Card.Title>
+                                            <Button variant="primary">Go somewhere</Button>
+                                        </Card.Body>
+                                    </Card>
+                                    </Col>
+                                    )
+                                })
+                            }
 
-                        </div>
+
+                        </Row>
                     </div>
-                    <SearchBar></SearchBar>
+                    
                 </section>
             </main>
         </>
     )
 }
 
-export default Popular;
+export default Movie_Popular;
