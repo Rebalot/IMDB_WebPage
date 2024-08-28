f033ca0979c60bd5745fdbc971bcf14a3303c139
fcoaguilarjt <fcoaguilar.jt@gmail.com>
2024-08-27 21:44:16 -0600

    Se añade now playing

--- src/routes/RoutesIndex.jsx
+++ src/routes/RoutesIndex.jsx
@@ -2,6 +2,8 @@ import { Routes, Route } from 'react-router-dom'
 import Home from '../pages/Home';
 import MoviePopular from '../pages/Movie_Popular';
 import DetallePelicula from '../pages/DetallePelicula';
+import NowPlaying from '../pages/NowPlaying';
+
 
 
 const RoutesIndex = () => {
@@ -10,6 +12,7 @@ const RoutesIndex = () => {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/movie/popular' element={<MoviePopular />} />
+            <Route path='/movie/now-playing' element={<NowPlaying />} />
             <Route path='/:typeDetail/:id' element={<DetallePelicula />} />
         </Routes>
     )
